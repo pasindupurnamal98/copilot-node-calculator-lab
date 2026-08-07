d0cf74d84a35c5303f76eeceaf99de73be0d7cfb
ttallis-insight <105190682+ttallis-insight@users.noreply.github.com>
2026-08-07 16:09:21 +1000

    Dev/update (#1)
    * update packages
    
    * update core functionality

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ app.use(express.static('public'));
 const routes = require("./api/routes");
 routes(app);
 
+app.use(function(err, _req, res, _next) {
+    res.status(400).json({ error: err.message });
+});
+
 if (!module.parent) {
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
+        console.log(`http://localhost:${port}`);
     });
 }
 
